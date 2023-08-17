206925231e4927d9cdd25e9b1eff1eb2c0410dba
juwon334 <juwon334@gmail.com>
2023-08-17 21:30:48 +0900

    test1

---
+++ popup.js
@@ -0,0 +1,27 @@
+document.getElementById('connectButton').addEventListener('click', async () => {
+    const accountNumber = document.getElementById('accountNumber').value;
+
+    try {
+        const response = await fetch('http://localhost:8080/checkAccount', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({ "accountNumber": accountNumber })
+        });
+
+        if (!response.ok) {
+            throw new Error("API 응답 오류");
+        }
+
+        const data = await response.json();
+        if (data.status === 'found') {
+            alert("계좌 발견: " + data.data);
+        } else {
+            alert("계좌를 찾을 수 없습니다.");
+        }
+    } catch (error) {
+        alert("API와 연결할 수 없습니다.");
+    }
+});
