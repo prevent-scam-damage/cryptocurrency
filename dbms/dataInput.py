217919469a263b33f1a93ae3d5c5f56ad34ba14d
choam2426 <choam2426@gmail.com>
2023-08-18 01:03:26 +0900

    db update

--- dbms/dataInput.py
+++ dbms/dataInput.py
@@ -1,18 +1,22 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi import FastAPI
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from urllib.parse import quote_plus
 from config import conf
 
+
 def insert_data():
-    # MongoDB 연결 설정
     MONGODB_URL = conf["DB_URL"]
     password = quote_plus(conf["password"])
     MONGODB_URL = MONGODB_URL.replace("password", password)
-    client = AsyncIOMotorClient(MONGODB_URL, server_api=ServerApi('1'))
-    db = client["tonDB"]  # 데이터베이스 선택
-    # 데이터 입력
-    data = {"name": "John", "age": 30}
-    db.blacklist.insert_one(data)
+    client = MongoClient(MONGODB_URL, server_api=ServerApi('1'))
+    db = client["tonDB"]
+    path = './blacklist.txt'
+    with open(path, 'r') as mainconf:
+        data = list(set(list(mainconf.read().split("\n"))))
+        for i in data:
+            input = {"address": i.strip()}
+            db.blacklist.insert_one(input)
     print("Data inserted successfully")
     # 연결 종료
     client.close()
